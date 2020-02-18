60a9a9224b5442e8ed72eeaea18131af0e1ec7b0
ZeroDawn0D <umang26112003@gmail.com>
2020-02-18 14:40:33 +0530

    CSS reorganized. Bio added.

--- blog.js
+++ blog.js
@@ -1,5 +1,4 @@
- 
-document.getElementById("blog").onclick = function openBlog()
+ document.getElementById("blog").onclick = function openBlog()
  {
 	if(confirm("DO you want to leave the site"))
 		window.open("https://semanticpsyduck.000webhostapp.com/");
