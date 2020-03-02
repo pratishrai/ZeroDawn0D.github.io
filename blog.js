9bb632a2eb9a593de7da48f49067ae6dfb11ff52
ZeroDawn0D <umang26112003@gmail.com>
2020-03-02 20:24:41 +0530

    Dark mode added, resume file added

--- blog.js
+++ blog.js
@@ -1,5 +1,10 @@
- document.getElementById("blog").onclick = function openBlog()
+ document.getElementsByClassName("blog1").onclick = function openBlog()
  {
 	if(confirm("DO you want to leave the site"))
 		window.open("https://semanticpsyduck.000webhostapp.com/");
- } 
+ }
+ document.getElementsByClassName("blog2").onclick = function openBlog()
+ {
+	if(confirm("DO you want to leave the site"))
+		window.open("https://medium.com/@umang26112003");
+ }
